df830e6c56985619c3aa5f28fca7763ea2fadcec
AntonellaTorr <67662917+AntonellaTorr@users.noreply.github.com>
2023-10-07 20:41:04 -0300

    Agregando animaciones

--- web/index.js
+++ web/index.js
@@ -1,14 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let sliderInner = document.querySelector(".slider-iner");
-    let images = sliderInner.querySelectorAll("img");
-    let index = 0;
+  let sliderInner = document.querySelector(".slider-iner");
+  let images = sliderInner.querySelectorAll("img");
+  let index = 0;
 
-    setInterval(function () {
-        index++;
-        if (index >= 2) {
-            index = 0;
-        }
-        let percentage = index * -100;
-        sliderInner.style.transform = "translateX(" + percentage + "%)";
-    }, 3000); // Cambia la imagen cada 3 segundos (ajusta el tiempo según tus necesidades)
+  setInterval(function () {
+    index++;
+    if (index >= 2) {
+      index = 0;
+    }
+    let percentage = index * -100;
+    sliderInner.style.transform = "translateX(" + percentage + "%)";
+  }, 3000); // Cambia la imagen cada 3 segundos (ajusta el tiempo según tus necesidades)
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Tu código JavaScript aquí
+  // Obtén el elemento de texto invisible
+  var textoInvisible = document.querySelector(".texto-invisible");
+
+  console.log(textoInvisible);
+
+  // Función para verificar la visibilidad del texto
+  function verificarVisibilidad() {
+    // Obtén la posición de desplazamiento vertical actual
+    var scrollPos = window.scrollY;
+
+    // La altura a la que quieres que aparezca el texto
+    var triggerPos =
+      window.innerHeight *
+      0.7; /* Aparecerá cuando el 70% de la ventana esté visible */
+
+    // Verifica si el usuario ha desplazado lo suficiente para mostrar el texto
+    if (scrollPos >= triggerPos) {
+      textoInvisible.style.opacity = "1"; // Hace que el texto sea visible
+    }
+  }
+
+  // Agrega un evento de desplazamiento a la ventana
+  window.addEventListener("scroll", verificarVisibilidad);
+
+  // Llama a verificarVisibilidad() al cargar la página para manejar el texto inicialmente visible si está cerca de la parte superior de la página
+  window.addEventListener("load", verificarVisibilidad);
 });
