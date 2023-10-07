0f0895af5cc990841c3a48b03a72579aec1f99c0
AntonellaTorr <67662917+AntonellaTorr@users.noreply.github.com>
2023-10-07 01:03:28 -0300

    Carrusel en funcionamiento

--- web/index.js
+++ web/index.js
@@ -1,16 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let sliderInner = document.querySelector(".slider-iner");
+    let images = sliderInner.querySelectorAll("img");
+    let index = 0;
 
-
-
-let sliderInner= document.querySelector(".slider-iner");
-let images=sliderInner.querySelectorAll("img");
-
-let index=1;
-
-setInterval(function(){
-    let percentage=index*-100;
-    sliderInner.style.transform= "translateX("+percentage+"%)"; 
-    index++;
-    if (index>images.length-1){
-        index=1;
-    }
-},1000);
+    setInterval(function () {
+        index++;
+        if (index >= 2) {
+            index = 0;
+        }
+        let percentage = index * -100;
+        sliderInner.style.transform = "translateX(" + percentage + "%)";
+    }, 3000); // Cambia la imagen cada 3 segundos (ajusta el tiempo según tus necesidades)
+});
